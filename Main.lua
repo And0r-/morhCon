ca10ed257daa73a2573158692a05c61763d7759a
And0r <ahabegger@gmail.com>
2020-06-20 14:47:08 +0200

    comment out all presentation stuff to build a first alpha version

--- Main.lua
+++ Main.lua
@@ -1,186 +1,186 @@
-local MCA = LibStub("AceAddon-3.0"):NewAddon("MorphConAddon")
+--local MCA = LibStub("AceAddon-3.0"):NewAddon("MorphConAddon")
 
 
-function MCA:OnInitialize()
-    -- Called when the addon is loaded
-end
+--function MCA:OnInitialize()
+--    -- Called when the addon is loaded
+--end
 
-function MCA:OnEnable()
-	-- Called when the addon is enabled
-	print("Addon geladen")
-end
-
-
------------------------------------------------------------------------------------------------
--- Add Console
+--function MCA:OnEnable()
+--	-- Called when the addon is enabled
+--	print("Addon geladen")
+--end
 
 
-local MCA = LibStub("AceAddon-3.0"):NewAddon("MorphConAddon", "AceConsole-3.0")
+-------------------------------------------------------------------------------------------------
+---- Add Console
 
 
-function MCA:OnInitialize()
-    -- Called when the addon is loaded
-    -- ++++++++++++
-	self:RegisterChatCommand("rl", function() ReloadUI() end)
-end
-
-function MCA:OnEnable()
-	-- Called when the addon is enabled
-	self:Print("Addon geladen")
-end
-
--- show event 1
-
------------------------------------------------------------------------------------------------
--- Add Event
-
-local MCA = LibStub("AceAddon-3.0"):NewAddon("MorphConAddon", "AceConsole-3.0", "AceEvent-3.0")
+--local MCA = LibStub("AceAddon-3.0"):NewAddon("MorphConAddon", "AceConsole-3.0")
 
 
-function MCA:OnInitialize()
-    -- Called when the addon is loaded
-	self:RegisterChatCommand("rl", function() ReloadUI() end)
-end
+--function MCA:OnInitialize()
+--    -- Called when the addon is loaded
+--    -- ++++++++++++
+--	self:RegisterChatCommand("rl", function() ReloadUI() end)
+--end
 
-function MCA:OnEnable()
-	-- Called when the addon is enabled
-	self:Print("Addon geladen")
-	self:RegisterEvent("BANKFRAME_OPENED")
-end
+--function MCA:OnEnable()
+--	-- Called when the addon is enabled
+--	self:Print("Addon geladen")
+--end
 
-function MCA:BANKFRAME_OPENED()
-	-- Get ItemID from first Bankslot
-	local itemId = GetContainerItemID(-1,1)
+---- show event 1
 
-	if itemId == 6948 then
-		self:Print("Gefunden")
-	else
-		self:Print("Woooo ist das Item?")
-	end
-end
+-------------------------------------------------------------------------------------------------
+---- Add Event
 
-------------------------------------------------------------------------------------------------------------------------
-
---Config Gui:
+--local MCA = LibStub("AceAddon-3.0"):NewAddon("MorphConAddon", "AceConsole-3.0", "AceEvent-3.0")
 
 
+--function MCA:OnInitialize()
+--    -- Called when the addon is loaded
+--	self:RegisterChatCommand("rl", function() ReloadUI() end)
+--end
 
-local MCA = LibStub("AceAddon-3.0"):NewAddon("MorphConAddon", "AceConsole-3.0", "AceEvent-3.0")
-MCA.db = {itemId=6948}
+--function MCA:OnEnable()
+--	-- Called when the addon is enabled
+--	self:Print("Addon geladen")
+--	self:RegisterEvent("BANKFRAME_OPENED")
+--end
 
-local options = { 
-	type = "group",
-	handler = MCA,
-	args = {
-		item = {
-			name = "ItemId",
-			desc = "Bitte nur ItemId eingeben",
-			type = "input",
-			get = "GetItemId",
-			set = "SetItemId",
-		},
-  	},
-}
+--function MCA:BANKFRAME_OPENED()
+--	-- Get ItemID from first Bankslot
+--	local itemId = GetContainerItemID(-1,1)
 
+--	if itemId == 6948 then
+--		self:Print("Gefunden")
+--	else
+--		self:Print("Woooo ist das Item?")
+--	end
+--end
 
-function MCA:GetItemId(info)
-	return tostring(self.db.itemId)
-end
+--------------------------------------------------------------------------------------------------------------------------
 
-function MCA:SetItemId(info, value)
-	self.db.itemId = tonumber(value)
-end
-
-function MCA:OnInitialize()
-    -- Called when the addon is loaded
-	self:RegisterChatCommand("rl", function() ReloadUI() end)
-
-    LibStub("AceConfig-3.0"):RegisterOptionsTable("MorphConFig", options)
-    self.optionsFrame = LibStub("AceConfigDialog-3.0"):AddToBlizOptions("MorphConFig", "MorphConAddon")
-end
-
-function MCA:OnEnable()
-	-- Called when the addon is enabled
-	self:Print("Addon geladen")
-	self:RegisterEvent("BANKFRAME_OPENED")
-end
+----Config Gui:
 
 
-function MCA:BANKFRAME_OPENED()
-	-- Get ItemID from first Bankslot
-	local itemId = GetContainerItemID(-1,1)
 
-	if itemId == self.db.itemId then
-		self:Print("Gefunden")
-	else
-		self:Print("Woooo ist das Item?")
-	end
-end
+--local MCA = LibStub("AceAddon-3.0"):NewAddon("MorphConAddon", "AceConsole-3.0", "AceEvent-3.0")
+--MCA.db = {itemId=6948}
 
-
------------------------------------------------------------------------------------------------------------------------------------------
-
-
--- Database:
-
-local MCA = LibStub("AceAddon-3.0"):NewAddon("MorphConAddon", "AceConsole-3.0", "AceEvent-3.0")
-
-local dbDefaults = {
-	profile = {
-		itemId = 6948
-	}
-}
-
-local options = { 
-	type = "group",
-	handler = MCA,
-	args = {
-		item = {
-			name = "ItemId",
-			desc = "Bitte nur ItemId eingeben",
-			type = "input",
-			get = "GetItemId",
-			set = "SetItemId",
-		},
-  	},
-}
+--local options = { 
+--	type = "group",
+--	handler = MCA,
+--	args = {
+--		item = {
+--			name = "ItemId",
+--			desc = "Bitte nur ItemId eingeben",
+--			type = "input",
+--			get = "GetItemId",
+--			set = "SetItemId",
+--		},
+--  	},
+--}
 
 
-function MCA:GetItemId(info)
-	return tostring(self.db.profile.itemId)
-end
+--function MCA:GetItemId(info)
+--	return tostring(self.db.itemId)
+--end
 
-function MCA:SetItemId(info, value)
-	self.db.profile.itemId = tonumber(value)
-end
+--function MCA:SetItemId(info, value)
+--	self.db.itemId = tonumber(value)
+--end
 
-function MCA:OnInitialize()
-    -- Called when the addon is loaded
-	self:RegisterChatCommand("rl", function() ReloadUI() end)
+--function MCA:OnInitialize()
+--    -- Called when the addon is loaded
+--	self:RegisterChatCommand("rl", function() ReloadUI() end)
 
-	self.db = LibStub("AceDB-3.0"):New("MCADB", dbDefaults, true)
+--    LibStub("AceConfig-3.0"):RegisterOptionsTable("MorphConFig", options)
+--    self.optionsFrame = LibStub("AceConfigDialog-3.0"):AddToBlizOptions("MorphConFig", "MorphConAddon")
+--end
 
-    LibStub("AceConfig-3.0"):RegisterOptionsTable("MorphConFig", options)
-    self.optionsFrame = LibStub("AceConfigDialog-3.0"):AddToBlizOptions("MorphConFig", "MorphConAddon")
-end
-
-function MCA:OnEnable()
-	-- Called when the addon is enabled
-	self:Print("Addon geladen")
-	self:RegisterEvent("BANKFRAME_OPENED")
-end
+--function MCA:OnEnable()
+--	-- Called when the addon is enabled
+--	self:Print("Addon geladen")
+--	self:RegisterEvent("BANKFRAME_OPENED")
+--end
 
 
-function MCA:BANKFRAME_OPENED()
-	-- Get ItemID from first Bankslot
-	local itemId = GetContainerItemID(-1,1)
+--function MCA:BANKFRAME_OPENED()
+--	-- Get ItemID from first Bankslot
+--	local itemId = GetContainerItemID(-1,1)
 
-	if itemId == self.db.profile.itemId then
-		self:Print("Gefunden")
-	else
-		self:Print("Woooo ist das Item?")
-	end
-end
+--	if itemId == self.db.itemId then
+--		self:Print("Gefunden")
+--	else
+--		self:Print("Woooo ist das Item?")
+--	end
+--end
+
+
+-------------------------------------------------------------------------------------------------------------------------------------------
+
+
+---- Database:
+
+--local MCA = LibStub("AceAddon-3.0"):NewAddon("MorphConAddon", "AceConsole-3.0", "AceEvent-3.0")
+
+--local dbDefaults = {
+--	profile = {
+--		itemId = 6948
+--	}
+--}
+
+--local options = { 
+--	type = "group",
+--	handler = MCA,
+--	args = {
+--		item = {
+--			name = "ItemId",
+--			desc = "Bitte nur ItemId eingeben",
+--			type = "input",
+--			get = "GetItemId",
+--			set = "SetItemId",
+--		},
+--  	},
+--}
+
+
+--function MCA:GetItemId(info)
+--	return tostring(self.db.profile.itemId)
+--end
+
+--function MCA:SetItemId(info, value)
+--	self.db.profile.itemId = tonumber(value)
+--end
+
+--function MCA:OnInitialize()
+--    -- Called when the addon is loaded
+--	self:RegisterChatCommand("rl", function() ReloadUI() end)
+
+--	self.db = LibStub("AceDB-3.0"):New("MCADB", dbDefaults, true)
+
+--    LibStub("AceConfig-3.0"):RegisterOptionsTable("MorphConFig", options)
+--    self.optionsFrame = LibStub("AceConfigDialog-3.0"):AddToBlizOptions("MorphConFig", "MorphConAddon")
+--end
+
+--function MCA:OnEnable()
+--	-- Called when the addon is enabled
+--	self:Print("Addon geladen")
+--	self:RegisterEvent("BANKFRAME_OPENED")
+--end
+
+
+--function MCA:BANKFRAME_OPENED()
+--	-- Get ItemID from first Bankslot
+--	local itemId = GetContainerItemID(-1,1)
+
+--	if itemId == self.db.profile.itemId then
+--		self:Print("Gefunden")
+--	else
+--		self:Print("Woooo ist das Item?")
+--	end
+--end
 
 
 ----------------------------------------------------------------------------------------------------------------------
--- lua_grundlagen.lua
+++ lua_grundlagen.lua
@@ -1,114 +1,114 @@
-globale_variable = "Globales Hello World"
-local lokale_variable = "Lokales Hello World"
+--globale_variable = "Globales Hello World"
+--local lokale_variable = "Lokales Hello World"
 
--- show event 4
+---- show event 4
 
--- /run print(globale_variable)
--- /run print(lokale_variable)
-
-
--- /reload
-----------------------------------------------------------------------------
--- show event 4 ende
-local myArray = {"value1", "value2", "value3"}
-print("array: "..myArray[2])
-
--- Ausgabe: value2
--- show event 1
+---- /run print(globale_variable)
+---- /run print(lokale_variable)
 
 
+---- /reload
+------------------------------------------------------------------------------
+---- show event 4 ende
+--local myArray = {"value1", "value2", "value3"}
+--print("array: "..myArray[2])
 
-local myHash = {eins="hashValue1", zwei="hashValue2", drei="hashValue3"}
-
-print("hash: "..myHash["zwei"])
--- Kann man aber noch viel cooler schreiben:
-print("cool hash: "..myHash.zwei)
-
--- show event 2
-
-
--- In lua gibt es aber keine Hashes und Arrays sondern nur Tables...
-
-local myTable = {"value1", [2]="value2", drei="value3"}
+---- Ausgabe: value2
+---- show event 1
 
 
 
--------------------------------------------------------------------------------
--- Funktionen
+--local myHash = {eins="hashValue1", zwei="hashValue2", drei="hashValue3"}
 
-function machEtwas()
-	print("ich gebe etwas aus")
-end 
+--print("hash: "..myHash["zwei"])
+---- Kann man aber noch viel cooler schreiben:
+--print("cool hash: "..myHash.zwei)
 
-machEtwas()
+---- show event 2
+
+
+---- In lua gibt es aber keine Hashes und Arrays sondern nur Tables...
+
+--local myTable = {"value1", [2]="value2", drei="value3"}
+
+
 
 ---------------------------------------------------------------------------------
-local myTable = {"value1", [2]="value2", drei="value3"}
+---- Funktionen
 
-function myTable.machEtwas(table)
-	print("ich gebe etwas anderes aus: "..table.drei)
-end 
+--function machEtwas()
+--	print("ich gebe etwas aus")
+--end 
 
-myTable.machEtwas(myTable)
+--machEtwas()
 
+-----------------------------------------------------------------------------------
+--local myTable = {"value1", [2]="value2", drei="value3"}
 
-function myTable:machEtwas()
-	print("ich gebe etwas anderes aus: "..self.drei)
-end 
+--function myTable.machEtwas(table)
+--	print("ich gebe etwas anderes aus: "..table.drei)
+--end 
 
-myTable:machEtwas()
-
-
-
-
----------------------------------------------------------------------------------------------------
--- Return values
-
-function getValue()
-	return "zwei"
-end
-
-print(getValue())
+--myTable.machEtwas(myTable)
 
 
-function returnTable()
-	return {"eins","zwei"}
-end
+--function myTable:machEtwas()
+--	print("ich gebe etwas anderes aus: "..self.drei)
+--end 
 
-print(returnTable()[2])
+--myTable:machEtwas()
 
----------
 
-function returnMultipleParams()
-	return "eins", "zwei"
-end
+
+
+-----------------------------------------------------------------------------------------------------
+---- Return values
+
+--function getValue()
+--	return "zwei"
+--end
+
+--print(getValue())
+
+
+--function returnTable()
+--	return {"eins","zwei"}
+--end
+
+--print(returnTable()[2])
+
+-----------
+
+--function returnMultipleParams()
+--	return "eins", "zwei"
+--end
 	
-local eins, zwei = returnMultipleParams()
-print(zwei)
+--local eins, zwei = returnMultipleParams()
+--print(zwei)
 
-print(select(2,returnMultipleParams()))
+--print(select(2,returnMultipleParams()))
 
--------------------------------------------------------------------------------------------------------
-
-
--- loops:
-
-local myTable = {[0]="v0", "v1", [2]="v2", d="v3", [4]="v4"}
+---------------------------------------------------------------------------------------------------------
 
 
--- ipairs vs pairs
+---- loops:
 
-for index, value in pairs(myTable) do
-	print(value)
-end
+--local myTable = {[0]="v0", "v1", [2]="v2", d="v3", [4]="v4"}
 
--- show event 5
 
-for index, value in ipairs(myTable) do
-	print(value)
-end
+---- ipairs vs pairs
 
--- show event 6
+--for index, value in pairs(myTable) do
+--	print(value)
+--end
+
+---- show event 5
+
+--for index, value in ipairs(myTable) do
+--	print(value)
+--end
+
+---- show event 6
 
 
 
